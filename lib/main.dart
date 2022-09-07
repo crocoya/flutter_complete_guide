ccd06660072c5b15a781e7a8ed945619027c289a
Crocoya <yassine.tababi@outlook.com>
2022-09-07 15:45:41 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,48 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('Hello!'),
-    );
+    return MaterialApp(
+        theme: ThemeData(
+            appBarTheme: const AppBarTheme(
+          color: Color.fromARGB(255, 204, 0, 0),
+        )),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(title: const Text('onepoint.'), actions: <Widget>[
+            IconButton(
+                icon: const Icon(Icons.add_alert),
+                tooltip: 'Show Snackbar',
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('This is a snackbar')));
+                }),
+            IconButton(
+                icon: const Icon(Icons.navigate_next),
+                tooltip: 'Go to the next page',
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute<void>(
+                    builder: (BuildContext context) {
+                      return Scaffold(
+                        appBar: AppBar(
+                          title: const Text('Next page'),
+                        ),
+                        body: const Center(
+                          child: Text(
+                            'This is the next page',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                        ),
+                      );
+                    },
+                  ));
+                })
+          ]),
+          body: const Center(
+            child: Text(
+              'Beyond the obvious',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ));
   }
 }
